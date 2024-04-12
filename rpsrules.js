475ab161ae10f82fff46853763503d6f624d1fd1
Kevin Lee <145483343+cowgoose@users.noreply.github.com>
2024-04-12 10:11:22 -0700

    added reset button and base case

--- rpsrules.js
+++ rpsrules.js
@@ -18,40 +18,60 @@ function getComputerChoice(){
 
 //game 
 function playRound(playerChoice, computerChoice){
-    let playerResponse = prompt("Please type rock, paper, or scissors!").toLowerCase();
     let computerResponse = getComputerChoice();
+    let playerResponse;
+    let counter = 0;
 
-    //base case
-    if(playerResponse != "rock" || "paper" || "scissors"){
-        alert("Please type in the correct format.");
-        playerResponse = prompt("Please type rock, paper, or scissors!").toLowerCase();
-    }
 
-    if(playerResponse == computerResponse){
-        console.log("Try again");
-        return alert("A tie! Try again");
     
+    //base case
+    do{
+        playerResponse = prompt("Please type rock, paper, or scissors!").toLowerCase(); 
+        
+        if(playerResponse != "rock" && playerResponse !=  "paper" && playerResponse !=  "scissors"){
+            alert("please put the correct choice");
+            counter = 0;
+        } 
     }
-    else if(playerResponse == "rock" && computerResponse == "paper"){
-        console.log("rockpaper");
-        return alert("Computer Wins!")
-    }
-    else if(playerResponse == "paper" && computerResponse == "scissors"){
-        console.log("paperscissors");
-        return alert("Computer Wins!")
-    }
-    else if(playerResponse == "scissors" && computerResponse == "rock"){
-        console.log("scissorsrock");
-        return alert("Computer Wins!")
-    }
-    else{
-        console.log("otherwin");
-        return alert("You win! Congrats!")
+    while(playerResponse != "rock" && playerResponse !=  "paper" && playerResponse !=  "scissors"){
+
+        if(playerResponse == computerResponse){
+            console.log("Try again");
+            return alert("A tie! Try again");
+        
+        }
+        else if(playerResponse == "rock" && computerResponse == "paper"){
+            console.log("rockpaper");
+            return alert("Computer Wins!")
+        }
+        else if(playerResponse == "paper" && computerResponse == "scissors"){
+            console.log("paperscissors");
+            return alert("Computer Wins!")
+        }
+        else if(playerResponse == "scissors" && computerResponse == "rock"){
+            console.log("scissorsrock");
+            return alert("Computer Wins!")
+        }
+        else{
+            console.log("otherwin");
+            counter++;
+            return alert("You win! Congrats!")
+        }
+     
     }
 
 }
 
-
 const playerSelection = "rock";
+
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+//restart button
+const restart = document.getElementById("restart");
+restart.addEventListener("click", function() {
+    counter = 0;
+    playRound();
+});
+
+
